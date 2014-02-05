615de627fd77630148e717ab2b30a510a49f3464
Joshua Kennedy <josh@deadmeta4.com>
2014-02-05 11:13:37 -0800

    placeholder for enyo

--- nodejs/madtags-webserver.js
+++ nodejs/madtags-webserver.js
@@ -2,10 +2,16 @@
 	
 	module.exports = function( app ){
 		app.get( '/', getRoot );
+		app.get( '/tv', getTV );
+
 	}
 
 	function getRoot( req, res ){
 		res.render('index');
 	}
 
+	function getTV( req, res ){
+		res.render('index');
+	}
+	
 })();
