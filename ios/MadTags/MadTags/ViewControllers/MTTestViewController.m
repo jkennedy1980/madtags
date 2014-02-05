d4c8a87f176418ee661f7ea16913c6a6024a5999
scott <scott@ponycode.com>
2014-02-05 11:45:20 -0800

    socket.IO

--- ios/MadTags/MadTags/ViewControllers/MTTestViewController.m
+++ ios/MadTags/MadTags/ViewControllers/MTTestViewController.m
@@ -21,10 +21,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	self.rocket = [[SRWebSocket alloc] initWithURL:[NSURL URLWithString:@"http://localhost"]];
+	NSURL *url = [NSURL URLWithString:@"ws://localhost:80"];
+	self.rocket = [[SRWebSocket alloc] initWithURL:url];
+	self.rocket.delegate = self;
+	[self.rocket open];
 }
 
 - (IBAction)didClickSendPing:(id)sender {
+	[self.rocket send:[NSString stringWithFormat:@"join"]];
+}
+
+#pragma mark - Rocket Delegate
+
+// message will either be an NSString if the server is using text
+// or NSData if the server is using binary.
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message;
+{
+	NSString *leMessage = (NSString*) message;
+	NSLog(@"Did Receive Message: %@", leMessage );
+}
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket;
+{
+	NSLog(@"Socket did open: %@", webSocket );
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error;
+{
+	NSLog( @"Shit a hamster: %@, %@", webSocket, error );
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean;
+{
+	NSLog( @"Did close socket: %@, code=%d, reason=%@, clean:%d", webSocket, (int)code, reason, wasClean );
 }
 
 @end
