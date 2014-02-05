e09c422162571cd8709dc6baff5e00871678cd0f
Joshua Kennedy <josh@deadmeta4.com>
2014-02-05 13:24:39 -0800

    Connect socketses on relaunch app

---
+++ ios/MadTags/MadTags/MTSocketWrapper.m
@@ -0,0 +1,91 @@
+//
+//  MTSocketWrapper.m
+//  MadTags
+//
+//  Created by Josh Kennedy on 2/5/14.
+//  Copyright (c) 2014 Scott Eklund. All rights reserved.
+//
+
+#import "MTSocketWrapper.h"
+#import "SocketIO.h"
+#import "SocketIOPacket.h"
+
+@interface MTSocketWrapper ()<SocketIODelegate>
+
+@property (nonatomic, strong) SocketIO *socket;
+
+@end
+
+
+@implementation MTSocketWrapper
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.socket = [[SocketIO alloc] initWithDelegate:self];
+    }
+    return self;
+}
+
+-(void) connect;
+{
+    if( !self.socket.isConnected ){
+        [self.socket connectToHost:@"JOSHs-MacBook-Pro.local" onPort:80];
+    }
+}
+
+-(void) disconnect;
+{
+    [self.socket disconnect];
+}
+
+-(void) joinGameWithCode:(NSString*) gameCode username:(NSString*) username;
+{
+    [self.socket sendEvent:@"joinClient" withData:@{ @"username": @"Josh Kennedy", @"gameCode": @"1234" }];
+}
+
+#pragma mark - SocketIO Delegate
+
+- (void) socketIODidConnect:(SocketIO *)socket;
+{
+	NSLog( @"Did Connect");
+    [self.delegate didConnect];
+}
+
+- (void) socketIODidDisconnect:(SocketIO *)socket disconnectedWithError:(NSError *)error;
+{
+	NSLog( @"Did Disconnect");
+    [self.delegate didDisconnect];
+}
+
+- (void) socketIO:(SocketIO *)socket didReceiveMessage:(SocketIOPacket *)packet;
+{
+	NSLog( @"Did Receive");
+}
+
+- (void) socketIO:(SocketIO *)socket didReceiveJSON:(SocketIOPacket *)packet;
+{
+	NSLog( @"didReceiveJSON");
+}
+
+- (void) socketIO:(SocketIO *)socket didReceiveEvent:(SocketIOPacket *)packet;
+{
+	NSLog( @"didReceiveEvent");
+	NSDictionary *dict = [NSJSONSerialization JSONObjectWithData: [packet.data dataUsingEncoding:NSUTF8StringEncoding]
+                                                         options: NSJSONReadingMutableContainers
+                                                           error: nil];
+	NSLog( @"dict: %@", dict );
+}
+
+- (void) socketIO:(SocketIO *)socket didSendMessage:(SocketIOPacket *)packet;
+{
+	NSLog( @"didSendMessage");
+}
+
+- (void) socketIO:(SocketIO *)socket onError:(NSError *)error;
+{
+	NSLog( @"onError");
+}
+
+@end
--- ios/MadTags/MadTags/ViewControllers/MTViewController.m
+++ ios/MadTags/MadTags/ViewControllers/MTViewController.m
@@ -7,23 +7,65 @@
 //
 
 #import "MTViewController.h"
+#import "MTSocketWrapper.h"
 
-@interface MTViewController ()
+@interface MTViewController ()<MTSocketWrapperDelegate>
+
+@property (nonatomic,strong) MTSocketWrapper *wrapper;
 
 @end
+
 
 @implementation MTViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.wrapper = [[MTSocketWrapper alloc] init];
+    self.wrapper.delegate = self;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillEnterBackground) name:UIApplicationWillResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillEnterForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
+
+    [self appWillEnterForeground];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)dealloc
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+-(void) viewWillAppear:(BOOL)animated;
+{
+    [super viewWillAppear:animated];
+}
+
+-(void) appWillEnterForeground;
+{
+    [self.wrapper connect];
+}
+
+-(void) appWillEnterBackground;
+{
+    [self.wrapper disconnect];
+}
+
+
+#pragma mark - Wrapper Delegate
+
+-(void) didConnect;
+{
+    [self.wrapper joinGameWithCode:@"1234" username:@"Josh Kennedy"];
+}
+
+-(void) didDisconnect;
+{
+    
+}
+
+-(void) changeToGamePhase:(NSString*) gamePhase data:(NSDictionary*) dictionary;
+{
+    
 }
 
 @end
