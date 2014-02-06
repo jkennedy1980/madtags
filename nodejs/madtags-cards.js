faf7a5b9c557b84b51cac76bf73ba08c86b9b76d
scott <scott@ponycode.com>
2014-02-06 11:29:41 -0800

    new goods

--- nodejs/madtags-cards.js
+++ nodejs/madtags-cards.js
@@ -1,12 +1,36 @@
 ( function(){
 
 	var	cards = [
-			"I found a pile of <<WORD>> under your mother's bed",
-			"After a night of 50 cent shots, I was so <<WORD>>",
-			"Three hours later, and a big bag of <<WORD>>, all was right with the world",
-			"I saw put the <<WORD>> in your pants.  I'm just saying, I saw.",
-			"2, 4, 6, 8, who your <<WORD>>"
-		]
+		"<<WORD>> gives me butterflies in my stomach.",
+		"Well the tests came back, you don't have <<WORD>>.",
+		"Me an' my <<WORD>> gonna hang tonight.",
+		"Do you need me to give you a <<WORD>>?",
+		"Who needs food when you got <<WORD>>?",
+		"I got a <<WORD>> at the mall today. I feel great!",
+		"What you looking at, <<WORD>>?!",
+		"I heard <<WORD>> just got out of rehab.",
+		"<<WORD>> is the best anime ever! Ever!",
+		"It feels like I have 10 pounds of <<WORD>> inside me.",
+		"<<WORD>> was good before they got popular.",
+		"<<WORD>> is my mom’s favorite Tagalog game show!",
+		"<<WORD>> pulled my favorite video on Youtube!",
+		"I swear. I will <<WORD>> those terrorists.",
+		"Learn to embrace your inner <<WORD>>.",
+		"Oh man, you just got <<WORD>>’d.",
+		"Hey everybody, <<WORD>> at my place! Bring some beers!",
+		"Francis Ford Coppola hasn’t made a decent movie since <<WORD>>.",
+		"It finally rained yesterday. Must be the <<WORD>> effect.",
+		"The only reason i get up the morning is <<WORD>> and bacon.",
+		"Do you have any <<WORD>>? I need something to nosh on.",
+		"<<WORD>> is mass media’s way of tricking people into reproducing.",
+		"I heard <<WORD>> had an affair with Marilyn Monroe.",
+		"<<WORD>> was once in an episode of Doctor Who.",
+		"<<WORD>> goes to show anyone can be Internet famous.",
+		"Who's prettier <<WORD>> or Benedict Cumberbatch?",
+		"I am standing up for <<WORD>> and slow clapping. Well played, sir. Well played.",
+		"Tonight is 50 cent shot night. We getting <<WORD>> wasted for sure!",
+		"One day, I was walking on the sidewalk when I bumped into <<WORD>>."
+	];
 
 	exports.cards = function(){
 		return cards;
