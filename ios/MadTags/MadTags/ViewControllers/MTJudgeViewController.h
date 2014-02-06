d209511cfd40726103a2aebc63d12dcb5963098c
Joshua Kennedy <josh@deadmeta4.com>
2014-02-05 21:56:38 -0800

    ch-ch-changes

---
+++ ios/MadTags/MadTags/ViewControllers/MTJudgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTJudgeViewController.h
+//  MadTags
+//
+//  Created by Josh Kennedy on 2/5/14.
+//  Copyright (c) 2014 Scott Eklund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTJudgeViewController : UIViewController
+
+@end
---
+++ ios/MadTags/MadTags/ViewControllers/MTJudgeViewController.m
@@ -0,0 +1,22 @@
+//
+//  MTJudgeViewController.m
+//  MadTags
+//
+//  Created by Josh Kennedy on 2/5/14.
+//  Copyright (c) 2014 Scott Eklund. All rights reserved.
+//
+
+#import "MTJudgeViewController.h"
+
+@interface MTJudgeViewController ()
+
+@end
+
+@implementation MTJudgeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
